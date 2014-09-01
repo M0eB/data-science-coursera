edac1332d4be24686820fb73e86eb76f286573b8
Mohamed T. Ismail <mohamed.t.ismail@gmail.com>
2014-08-31 22:26:19 -0400

    3 - Getting Cleaning Data - Adding Week 1 material

---
+++ 3-getting-cleaning-data/week-1-getting-data.R
@@ -0,0 +1,173 @@
+
+
+
+
+createSubDir <- function( subDir )
+{
+      mainDir <- getwd()
+      dir.create(file.path(mainDir, subDir), showWarnings = FALSE)
+}
+
+get_and_read_csv <- function()
+{
+      
+      if (!file.exists("data")) 
+      {
+            dir.create("data")
+      }
+      
+      if( !file.exists("./data/cameras.csv") )
+      {
+            ## Download file
+            fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
+            download.file( fileUrl, destfile = "./data/cameras.csv" )
+      
+            ## Save time file was downloaded
+            dateDownloaded <- date()
+      }
+      
+      ## Load file using read.table
+      cameraData <- read.table( "./data/cameras.csv", sep = ",", header = TRUE )
+      print ( head( cameraData ) )
+      
+      ## Load file using read.csv to simplify
+      cameraData <- read.csv( "./data/cameras.csv" )
+      print( head( cameraData ) )
+      
+}
+
+get_and_read_xls <- function()
+{
+      
+      if(!file.exists("data"))
+      {
+            dir.create("data")
+      }
+      
+      if( !file.exists("./data/cameras.xlsx" ) )
+      {
+            ## Download file
+            fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xlsx?accessType=DOWNLOAD"
+            download.file(fileUrl,destfile="./data/cameras.xlsx",method="curl")
+            
+            ## Save time file was downloaded
+            dateDownloaded <- date()
+      }
+      
+      ## Load xlsx file file
+      library( xlsx )
+      cameraData <- read.xlsx( "./data/cameras.xlsx", sheetIndex=1, header=TRUE )
+      head( cameraData )
+      
+      ## Read specific rows/columns
+      colIndex <- 2:3
+      rowIndex <- 1:4
+      cameraDataSubset <- read.xlsx( "./data/cameras.xlsx", sheetIndex=1, 
+                                     colIndex=colIndex, rowIndex=rowIndex )
+      cameraDataSubset
+}
+
+get_and_read_xml <- function()
+{ 
+      # Sample XML from http://www.w3schools.com/xml/simple.xml
+      # <breakfast_menu>
+      #     <food>
+      #        <name>Belgian Waffles</name>
+      #        <price>$5.95</price>
+      #        <description>Belgian Waffles with maple syrup</description>
+      #        <calories>650</calories>
+      #     </food>
+      #     ... more food nodes ...
+      # </breakfast_menu>
+      
+      library( XML )
+      
+      ## Read xml directly from website
+      fileUrl <- "http://www.w3schools.com/xml/simple.xml"
+      doc <- xmlTreeParse( fileUrl, useInternal=TRUE )
+      rootNode <- xmlRoot( doc )
+         
+      xmlName( rootNode )  # Get the name of the root node
+      names( rootNode )    # Get the names of the child nodes
+      
+      ## Extract Nodes
+      rootNode[[1]]        # <food> node
+      rootNode[[1]][[1]]   # <name> node
+      
+      # Get all items recursively
+      xmlSApply( rootNode, xmlValue )  
+      
+      ## Extract name values
+      names <- xpathSApply( rootNode, "//name", xmlValue )
+      print( names )
+      
+      ## Extract all price values
+      prices <- xpathSApply( rootNode, "//price", xmlValue )
+      print( prices )
+}
+
+get_and_read_xml_2 <- function()
+{
+      # Sample section form website
+      # <li class="team-name">
+      #     <a href="http://espn.go.com/nfl/team/_/name/wsh/washington-redskins">Washington</a>
+      # </li>
+      # <li class="score">
+      #     <a href="/nfl/recap?gameId=400554688">23-17</a>
+      # </li>
+      
+      ## Read xml directly from website
+      fileUrl <- "http://espn.go.com/nfl/team/_/name/bal/baltimore-ravens"
+      doc <- htmlTreeParse( fileUrl,useInternal=TRUE )
+      
+      ## Extract contents by attributes
+      scores <- xpathSApply(doc,"//li[@class='score']",xmlValue)
+      teams <- xpathSApply(doc,"//li[@class='team-name']",xmlValue)
+      print( scores )
+      print( teams )
+}
+
+get_and_read_json <- function()
+{
+      ##install.packages("jsonlite")
+      
+      library( jsonlite )
+      
+      ## Read json data directly form the web
+      jsonData <- fromJSON( "https://api.github.com/users/jtleek/repos" )
+      
+      names( jsonData )         #Extract names of main nodes
+      names( jsonData$owner )   #Extract inner nodes
+      jsonData$owner$login      #Go deeper...
+}
+
+from_and_to_json <- function()
+{
+      myjson <- toJSON( iris, pretty=TRUE )
+      print( "The JSON Data : ")
+      cat( myjson )
+
+      iris2 <- fromJSON( myjson )
+      print( "The Original Data : ")
+      head( iris2 )
+}
+
+create_data_table <- function()
+{
+      
+      library(data.table)
+      
+      DF = data.frame( x=rnorm(9), y=rep( c("a","b","c"), each=3), z=rnorm(9) )
+      head( DF, 3 )
+      
+      DF = data.table( x=rnorm(9), y=rep( c("a","b","c"), each=3), z=rnorm(9) )
+      head( DF, 3 )
+      
+      tables() # See all the data tables in memory
+      
+      # Yeaa booiiiii
+      
+}
+
+
+
---
+++ 3-getting-cleaning-data/week-1-practice.R
@@ -0,0 +1,113 @@
+
+
+
+
+question1 <- function()
+{
+      # The American Community Survey distributes downloadable data about
+      # United States communities.Download the 2006 microdata survey about
+      # housing for the state of Idaho using download.file() from here:       
+      # https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv 
+      
+      # The code book, describing the variable names is here:      
+      # https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf 
+      
+      # How many properties are worth $1,000,000 or more?
+      # a) 47    b) 31      c) 2076      d) 53
+      
+
+      if( !file.exists("./data/idaho_housing.csv") )
+      {
+            ## Download data
+            fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv" 
+            download.file( fileURL, destfile = "./data/idaho_housing.csv" )
+      }
+      
+      
+      if ( !file.exists("./data/idaho_housing_codebook.pdf") )
+      {
+            ## Download codebook
+            codebookURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf"
+            download.file( codebookURL, destfile = "./data/idaho_housing_codebook.pdf")
+      }
+      
+      ## Load data 
+      housingData <- read.csv("./data/idaho_housing.csv")
+      
+      ## Get the property value (called VAL according to codebook )
+      propertyValue <- housingData$VAL
+      propertyValue <- na.omit( propertyValue )     
+      
+      ## Count properties worth $1M or more
+      length( propertyValue[propertyValue >= 24] )  # 24 = $1M+ in codebook
+      
+}
+
+question3 <- function()
+{
+      # Download the Excel spreadsheet on Natural Gas Aquisition Program here:     
+      # https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx 
+      
+      # Read rows 18-23 and columns 7-15 into R and assign the result to a variable called:  dat 
+      # What is the value of:  sum(dat$Zip*dat$Ext,na.rm=T) ?
+      # a) 36534720     b) 0     c) 154339     d) 338924 
+           
+      if( !file.exists("./data/natural_gas_data.xlsx") )
+      {
+            ## Download data
+            fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx" 
+            download.file( fileURL, destfile = "./data/natural_gas.xlsx" )
+      }
+      
+      library( xlsx )
+      gasData <- read.xlsx( "./data/natural_gas_data.xlsx", sheetIndex=1, header=FALSE )
+      head( gasData )
+      
+      ## Read specific rows/columns
+      colIndex <- 7:15
+      rowIndex <- 18:23
+      dat <- read.xlsx( "./data/natural_gas_data.xlsx", sheetIndex=1, 
+                        colIndex=colIndex, rowIndex=rowIndex )
+      
+      sum( dat$Zip * dat$Ext, na.rm = TRUE ) # Multiply the two columns and sum the results...
+      
+}
+
+
+
+question4 <- function()
+{
+      # Read the XML data on Baltimore restaurants from here:      
+      # https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml 
+      # How many restaurants have zipcode 21231?
+
+      library( XML )
+      
+      ## Read xml directly from website
+      dataURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+      download.file( dataURL, destfile="./data/restaurants.xml")
+      
+      if( !file.exists("./data/restaurants.xml") )
+      {
+            ## Download data
+            fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+            download.file( fileURL, destfile = "./data/restaurants.xml" )
+      }
+      
+      doc <- xmlInternalTreeParse( "./data/restaurants.xml" )
+      rootNode <- xmlRoot( doc )
+      
+      ## Get all zipcode values that match
+      names <- xpathSApply( rootNode, "//row/zipcode", xmlValue )
+      length( names[ names == "21231" ] )
+}
+
+
+
+question5 <- function()
+{
+      
+      
+}
+
+
