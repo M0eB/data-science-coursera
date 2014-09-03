9cb40c40a9ab68c390ed5ade2969f89861d2759d
Mohamed T. Ismail <mohamed.t.ismail@gmail.com>
2014-09-02 23:02:48 -0400

    3 - Getting Cleaning Data - Adding week 2 examples for hdf5, web, apis

--- 3-getting-cleaning-data/week-2-getting-data.R
+++ 3-getting-cleaning-data/week-2-getting-data.R
@@ -1,6 +1,6 @@
 
 
-## RESOURCES ===================================================
+## mySQL RESOURCES ============================================================
 ##
 ## RMySQL vignette :
 ## http://cran.r-project.org/web/packages/RMySQL/RMySQL.pdf
@@ -11,9 +11,7 @@
 ## Summary of Commands :
 ## http://www.r-bloggers.com/mysql-and-r/
 ##
-## =============================================================
-
-
+## ============================================================================
 
 get_mysql_data <- function()
 {
@@ -82,22 +80,155 @@ get_mysql_data <- function()
       
 }
 
-get_HDF5_data <- function()
+
+## HDF5 RESOURCES =============================================================
+##
+## hdf5 can be used to optimize reading/writing disc in R
+##
+## rhdf5 Tutorial :
+## www.bioconductor.org/packages/release/bioc/vignettes/rhdf5/inst/doc/rhdf5.pdf
+##
+## HDF group has information on HDF5 in general :
+## http://www.hdfgroup.org/HDF5/
+##
+## ============================================================================
+
+get_hdf5_data <- function()
 {
+      source( "http://bioconductor.org/biocLite.R" )
+      biocLite( "rhdf5" )
       
+      library( rhdf5 )
+      
+      created = h5createFile( "example.h5" )
+      created
+      
+      ## This will install packages from Bioconductor http:///bioconductor.org/
+      ## primarily used for genomics but also has good "big data" packages
+      ## Can be used to interface with hdf5 data sets
+      ## This lecture is modeled very closely on the rhdf5 tutorial here :
+      ## http://www.bioconductor.org/packages/release/bioc/vignettes/rhdf5/inst/doc/rhdf5.pdf
+      
+      ## Create groups
+      created = h5createGroup( "example.h5", "foo" )
+      created = h5createGroup( "example.h5", "baa" )
+      created = h5createGroup( "example.h5", "foo/foobaa" )
+      h5ls( "example.h5" )
+      
+      ## Write to groups
+      A = matrix( 1:10, nr=5, nc=2 )
+      h5write( A, "example.h5", "foo/A" )
+      
+      B = array( seq( 0.1, 2.0, by=0.1 ), dim=c( 5, 2, 2) )
+      attr( B, "scale" ) <- "liter"
+      h5write( B, "example.h5", "foo/foobaa/B" )
+      h5ls( "example.h5" )
+      
+      ## Write a data set
+      df = data.frame( 1L:5L, seq( 0, 1, length.out=5 ),
+                       c( "ab", "cde", "fghi", "a", "s"), stringsAsFactors=FALSE )
+      h5write( df, "example.h5", "df" )
+      h5ls( "example.h5" )
+      
+      ## Reading Data
+      readA = h5read( "example.h5", "foo/A" )
+      readB = h5read( "example.h5", "foo/foobaa/B" )
+      readdf= h5read( "example.h5", "df" )
+      readA
+      
+      ## Writing and Reading Chunks
+      h5write( c( 12, 13, 14), "example.h5", "foo/A", index=list( 1:3, 1) )
+      h5read( "example.h5", "foo/A" )
       
 }
+
+
+## WEBSCAPING RESOURCES =======================================================
+## 
+## httr Package Documentation
+##    http://cran.r-project.org/web/packages/httr/httr.pdf
+##
+## - httr allows GET, POST, PUT, DELETE requests if you are authorized
+## - You can authenticate with a user name or password
+## - Most modern APIs use something like oauth
+## - httr works well with Facebook, Google, Twitter, Github, etc.
+##
+## R Bloggers has a number of examples of web scraping :
+##    http://www.r-bloggers.com/?s=Web+Scraping
+##
+## httr help files has useful examples :
+##    http://cran.r-project.org/web/packages/httr/httr.pdf
+##
+## ============================================================================
 
 get_web_data <- function()
 {
+      ## Getting Data off Webpages 
+      con = url( "http://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en" )
+      htmlCode = readLines( con )
+      close( con )
+      htmlCode
       
+      ## Parsing with XML
+      library( XML )
+      url <- "http://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en"
+      html <- htmlTreeParse( url, useInternalNodes=TRUE )
+      xpathSApply( html, "//title", xmlValue )  
+      
+      xpathSApply( html, "//td[@id='col-citedby']", xmlValue )
+
+      ## GET from httr Package
+      library( httr )
+      html2 = GET( url )
+      parseHtml = htmlParse( content2, asText=TRUE )
+      xpathSApply( parseHtml, "//title", xmlValue )
+      
+      ## Accessing Websites with Passwords
+      pg1 = GET( "http://httpbin.org/basic-auth/user/passwd" )
+      pg1
+      
+      pg2 = GET("http://httpbin.org/basic-auth/user/passwd",
+                authenticate("user","passwd"))
+      pg2
+      
+      names(pg2)
+      
+      ## Using Handles
+      google = handle("http://google.com")
+      pg1 = GET( handle=google, path="/" )
+      pg2 = GET( handle=google, path="search" )
       
 }
 
-get_API_data <- function()
+
+## API RESOURCES ==============================================================
+##
+## httr Package Documentation
+##    http://cran.r-project.org/web/packages/httr/httr.pdf
+##
+## - httr allows GET, POST, PUT, DELETE requests if you are authorized
+## - You can authenticate with a user name or password
+## - Most modern APIs use something like oauth
+## - httr works well with Facebook, Google, Twitter, Github, etc.
+##
+## Twitter REST API documentation :
+##    https://dev.twitter.com/docs/api/1.1/overview
+##    https://dev.twitter.com/docs/api/1.1/get/search/tweets
+##
+## ============================================================================
+
+get_twitter_api_data <- function()
 {
+      ## Accessing Twitter from R      
+      myapp = oauth_app( "twitter", key="yourConsumerKeyHere", secret="yourConsumerSecretHere" )
+      sig = sign_oauth1.0( myapp, token="yourTokenHere", token_secret="yourTokenSecretHere" )
+      homeTL = GET( "https://api.twitter.com/1.1/statuses/home_timeline.json", sig )
       
-      
+      ## Converting the JSON Object
+      json1 = content(homeTL)
+      json2 = jsonlite::fromJSON(toJSON(json1))
+      json2[1, 1:4]
+
 }
 
 
