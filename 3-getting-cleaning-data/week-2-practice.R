b620993096df27b6906242df09173154d963e9bb
Mohamed T. Ismail <mohamed.t.ismail@gmail.com>
2014-09-10 00:15:45 -0400

    3 - Getting Cleaning Data - Code/notes for using github api
    Added code and notes for using github api. Among other minor things.

--- 3-getting-cleaning-data/week-2-practice.R
+++ 3-getting-cleaning-data/week-2-practice.R
@@ -8,18 +8,83 @@
 
 question1 <- function()
 {
+      # Github API ------------------------------------------------------------
       # Register an application with the Github API here 
       # https://github.com/settings/applications. 
       # Access the API to get information on your instructors repositories 
-      # (hint: this is the url you want "https://api.github.com/users/jtleek/repos"). 
+      # (hint: URL to use is "https://api.github.com/users/jtleek/repos"). 
       # Use this data to find the time that the datasharing repo was created. 
       # What time was it created? 
-      
+      # 
       # This tutorial may be useful 
       # (https://github.com/hadley/httr/blob/master/demo/oauth2-github.r). 
       # You may also need to run the code in the base R package and not R studio.
+      # -----------------------------------------------------------------------
       
       
+      # Process ---------------------------------------------------------------
+      #
+      # 1. Install and loaded httr and httpuv packages
+      # 2. Find the OAuth Settings for Github
+      # 3. Register an application on github and get its ID and secret values
+      # 4. Use the application ID and secret to create an OAuth application
+      # 5. Get a token for the application
+      # 6. Set the token value using config() to do an HTTP GET request
+      # 7. Send a GET request to the github url you want info about
+      # 8. View the contents of the response from github using content()
+      #
+      # -----------------------------------------------------------------------
+      
+      install.packages(httr)      ## Tools for working with URLs and HTTP
+      install.packages(httpuv)    ## HTTP and WebSocket server library
+
+      library(httr)
+      library(httpuv)
+      
+      # REQUEST  : Get github endpoints ---------------------------------------
+      # RESPONSE :
+      #     <oauth_endpoint>
+      #       authorize: https://github.com/login/oauth/authorize
+      #       access:    https://github.com/login/oauth/access_token      
+      
+      oauth_endpoints("github")
+
+      
+      # Register an application at https://github.com/settings/applications;
+      #     Homepage URL : Use any URL you want (http://github.com is fine)
+      #     Callback URL : http://localhost:1410
+      # When registered you will get the application page with this info :
+      #     Client ID : 395302338dee4a26e78f
+      #     Client Secret : a8b16f3c03adc18233a27482c982ef0bd192d0b1
+      # Use the ID and secret to create the OAuth Application :
+
+      ## Create the OAuth Application
+      myapp <- oauth_app( "github",                      
+                         "395302338dee4a26e78f", 
+                         "a8b16f3c03adc18233a27482c982ef0bd192d0b1" )
+      
+      ## Get OAuth credentials
+      github_token <- oauth2.0_token( oauth_endpoints("github"), myapp )
+      
+      
+      # A new browser page should open allowing you to authenticate the app :
+      #    Console : "Waiting for authentication in browser..."
+      #    Browser : <Click to Authenticate app>
+      #    Browser : "Authentication complete. Please close this page and return to R."
+      #    Console : "Authentication complete."
+      
+      
+      ## Send get request and view response
+      gtoken <- config( token = github_token )                  
+      req <- GET( "https://api.github.com/users/jtleek/repos", gtoken )   
+      stop_for_status(req)
+      content( req )                                             
+      
+      # Alternate method...
+      req <- with_config( gtoken, GET("https://api.github.com/users/jtleek/repos") )
+      stop_for_status(req)
+      content(req)
+                 
 }
 
 
@@ -41,10 +106,6 @@ question2 <- function()
       # b) sqldf("select pwgtp1 from acs where AGEP < 50")
       # c) sqldf("select * from acs")
       # d) sqldf("select pwgtp1 from acs")
-      
-      
-      
-      
       
 }
 
@@ -68,10 +129,18 @@ question4 <- function()
       # How many characters are in the 10th, 20th, 30th and 100th 
       # lines of HTML from this page:      
       # http://biostat.jhsph.edu/~jleek/contact.html 
-      
       # (Hint: the nchar() function in R may be helpful)
+
+      ## Read webpage content
+      con = url( "http://biostat.jhsph.edu/~jleek/contact.html" )
+      htmlCode = readLines( con )
+      close( con )
       
-      
+      ## Count characters
+      print( nchar( htmlCode[10] ) )
+      print( nchar( htmlCode[20] ) )
+      print( nchar( htmlCode[30] ) )
+      print( nchar( htmlCode[100] ) )
       
 }
 
@@ -88,7 +157,11 @@ question5 <- function()
       
       # (Hint this is a fixed width file format)
 
+      fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for "
       
+      data <- read.fwf( file = url( fileURL ),
+                        skip = 4,
+                        widths = c(12, 7,4, 9,4, 9,4, 9,4) )  ##TODO: Fix these
       
 }
 
