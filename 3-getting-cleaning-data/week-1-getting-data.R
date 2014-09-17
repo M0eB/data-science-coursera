49bdb7f088bf5399f3b201e2e81541a9f208169f
Mohamed T. Ismail <mohamed.t.ismail@gmail.com>
2014-09-17 00:18:44 -0400

    3 - Getting Cleaning Data - data.table package notes

--- 3-getting-cleaning-data/week-1-getting-data.R
+++ 3-getting-cleaning-data/week-1-getting-data.R
@@ -163,20 +163,100 @@ from_and_to_json <- function()
 }
 
 
-create_data_table <- function()
+data_table_package <- function()
 {
-      
+      # data.table ------------------------------------------------------------
+      #     - Inherits from data.frame
+      #     - Written in C so much faster
+      #     - Much, much faster at subsetting, group, and updating
+      # -----------------------------------------------------------------------
+
       library(data.table)
+
+      ## Create data tables just like data frames
       
-      DF = data.frame( x=rnorm(9), y=rep( c("a","b","c"), each=3), z=rnorm(9) )
+      DT = data.frame( x=rnorm(9), y=rep( c("a","b","c"), each=3), z=rnorm(9) )
       head( DF, 3 )
-      
-      DF = data.table( x=rnorm(9), y=rep( c("a","b","c"), each=3), z=rnorm(9) )
+
+      DT = data.frame( x=rnorm(9), y=rep( c("a","b","c"), each=3), z=rnorm(9) )
       head( DF, 3 )
+
+      ## See all the data tables in memory
+      tables() 
       
-      tables() # See all the data tables in memory
-      
-      # Yeaa booiiiii
-      
+      ## Subsetting Rows
+      DT[ 2,0 ]               # Get a row
+      DT[ DT$y=="a", ]        # Get rows where y='a'
+      DT[ c(2,3) ]            # Using one elements subsets by row (rows 2,3)
+
+
+      ## Subsetting Columns
+      DT[,c(2,3)]
+
+
+      # Column Subsetting in data.table ---------------------------------------
+      #     - The subsetting function is modified for data.table
+      #     - The argument you pass after the comma is called an "expression"
+      #     - In R an expression is a collection of statements 
+      #       enclosed in curly brackets { } 
+      # -----------------------------------------------------------------------
+
+      {
+            x = 1
+            y = 2
+      }
+
+      k = { print(10); 5 }
+      print( k )
+
+
+      ## Calculating values for variables with expresions
+      DT[ , list(mean(x), sum(z)) ]
+      DT[ , table(y) ]
+
+      ## Adding new columns
+      DT[ , W:=Z^2 ]
+      DT2 <- DT
+      DT[ , y:=2 ]
+
+      ## Careful
+      head( DT, n=3 )
+      head( DT2, n=3 )
+
+      ## Multiple Operations
+      DT[ , m:={tmp <- (x+z); log(tmp+5)} ]
+
+
+      ## plyr like operations
+      DT[ , a:=x>0 ]
+      DT[ , b:=mean(x+w), by=a ]
+
+
+      ## Special Variables : .N An integer, length 1, containing the number r
+      set.seed( 123 )
+      DT <- data.table( x=sample( letters[1:3], 1E5, TRUE) )
+      DT[ , .N, by=x ]
+
+
+      ## Keys
+      DT <- data.table( x=rep( c("a","b","c"), each=100), y=rnorm(300) )
+      setkey( DT, x )
+      DT['a']
+
+      ## Joins
+      DT1 <- data.table( x=c('a', 'a', 'b', 'dt1'), y=1:4 )
+      DT2 <- data.table( x=c('a', 'b', 'dt2'), z=5:7 )
+      setkey( DT1, x ) 
+      setkey( DT2, x )
+      merge( DT1, DT2 )
+
+
+      ## Fast Reading
+      big_df <- data.frame( x=rnorm(1E6), y=rnorm(1E6) )
+      file <- tempfile()
+      write.table( big_df, file=file, row.names=FALSE, col.names=TRUE, sep="\t", quote=FALSE )
+      system.time( fread(file) )
+      system.time( read.table(file, header=TRUE, sep="\t") )
+
 }
 
