7ec494cf8eb1ebf28e943b20997193e0dbed6e71
Mohamed T. Ismail <mohamed.t.ismail@gmail.com>
2014-09-18 00:31:29 -0400

    4 - Exploratory Data Analysis - Week 2 - ggplot2 notes (half)

--- 4-exploratory-data-analysis/week-2-plotting.R
+++ 4-exploratory-data-analysis/week-2-plotting.R
@@ -110,14 +110,105 @@ lattice_plotting_system <- function()
 
 
 
+# The ggplot2 Plotting System -------------------------------------------------
+#
+# - An implementation of the Grammar of Graphics by Leland Wilkinson
+# - Grammar of graphics represents an abstraction of graphics ideas/objects
+# - Think "verb", "noun", "adjective" for graphics
+#        " In brief, the grammar tells us that a statistical 
+#          graphic is a mapping from data to aesthetic attributes
+#          (color, shape, size) of geometric objects (points, lines, bars).
+#          The plot may also contain statistical transformations
+#          of the data and is drawn on a specific coordinate system "
+#                     - from ggplot2 book
+# - Powerful and flexible if you learn the grammar and various elements
+# 
+#       *** NOTE : The code does not cover all types of plots ****
+#
+# Components of a Plot : 
+#     - Data Frame
+#     - aesthetic mappings : how data are mapped to color, size
+#     - geoms : geometric objects like points, lines, shapes
+#     - facets : for conditional plots
+#     - stats : statistical transformations like binning, quantiles, smoothing
+#     - scales : what scale an aesthetic map uses (ie- male=red, female=blue)
+#     - coordinate system
+#
+# Functions : 
+#            qplot() :  
+#           ggplot() :  
+#       geom_point() :  
+#        geom_line() : 
+#      geom_smooth() :  
+#       facet_grid() :  
+#       facet_wrap() :  
+#              aes() :  
+#             labs() : 
+#  coord_cartesian() :  
+#
+# -----------------------------------------------------------------------------
+
 ggplot2_plotting_system <- function()
 {
-      
-      
-      
-      
-      
-      
+      # Basics : qplot() ------------------------------------------------------
+      # 
+      #     - Works like plot in base graphics system
+      #     - Looks for data in a data frame or in parent environment
+      #     - Plots are made up of aesthetics (size, shape, color)
+      #       and geoms (points, lines)
+      #     - Factors are important for indicating subsets of the data
+      #     - hides what goes on underneath, which is ok for most operations
+
+      install.packages( ggplot2 )
+      library( ggplot2 )
+
+      str(mpg)  # The dataset we will work with
+
+      qplot( displ, hwy, data=mpg )              # x,y,dataframe
+      qplot( displ, hwy, data=mpg, color=drv )   # add color aesthetic
+      qplot( displ, hwy, data=mpg, geom = c("point", "smooth") )  # Add geom
+
+      qplot( hwy, data=mpg, fill=drv )           # Histogram
+
+
+      # Facets ----------------------------------------------------------------
+
+      qplot( displ, hwy, data=mpg, facets=.~drv ) 
+      qplot( hwy, data=mpg, facets=drv~., binwidth=2 )
+
+
+      # Histograms ------------------------------------------------------------
+
+      str( maacs )                                      # new dataset
+      qplot( log(eno), data=maacs )                     # Histogram 
+      qplot( log(eno), data=maacs, fill=mopos )         # color by group
+
+
+      # Density plots ---------------------------------------------------------
+
+      qplot( log(eno), data=maacs, geom="density" )               
+      qplot( log(eno), data=maacs, geom="density", color=mopos)   # By group
+
+
+      # Scatterplots ----------------------------------------------------------
+
+      qplot( log(pm25), log(eno), data=maacs )                 
+      qplot( log(pm25), log(eno), data=maacs, shape=mopos )    # group by shape
+      qplot( log(pm25), log(eno), data=maacs, color=mopos )    # group by color
+
+      # Group using color
+      qplot( log(pm25), log(eno), data=maacs, color=mopos, 
+                                              geom=c("point", "smooth"), 
+                                              method="lm" )
+
+      # Group using facets (separte plot for each group)
+      qplot( log(pm25), log(eno), data=maacs, geom=c("point", "smooth"),
+                                              method="lm",
+                                              facets=.~mopos )
+
+
+      ## HALFWAY...
+      ## NOTES AFTER THIS BUILD PLOT IN LAYERS, CONSIDER SEPARATE FUNCTION
 }
 
 
